1d69b01c59fb3359c924ef6cfb97ae5dfa115dd1
cyjadela <adelacho@naver.com>
2025-01-17 14:35:40 +0900

    Feat: / url 요청 시 성공 로직 추가

---
+++ src/main/java/com/example/demo/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("Users Application is Running!");
+    }
+}
