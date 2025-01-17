38bb914efc43d7dde5d7dc55f8cbc5f5c3b8fa5f
cyjadela <adelacho@naver.com>
2025-01-17 14:26:50 +0900

    Feat: RestTemplate으로 Users 조회 시 Goods 조회 API 호출하는 로직 구현

--- src/main/java/com/example/demo/service/GoodsService.java
+++ src/main/java/com/example/demo/service/GoodsService.java
@@ -1,8 +1,24 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.GoodsDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GoodsService {
+
+    private final RestTemplate restTemplate;
+    private final String goodsServiceUrl;
+
+    public GoodsService(@Value("${goods.service.url}") String goodsServiceUrl, RestTemplate restTemplate) {
+        this.goodsServiceUrl = goodsServiceUrl;
+        this.restTemplate = restTemplate;
+    }
+
+    public GoodsDto getGoodsByGoodsNo(String goodsNo) {
+        String url = goodsServiceUrl + "/api/v1/goods/" + goodsNo;
+        return restTemplate.getForObject(url, GoodsDto.class);
+    }
 
 }
