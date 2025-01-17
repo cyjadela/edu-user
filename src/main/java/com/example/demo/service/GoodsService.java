85787b6af5903388eb2c356e6996c9e8c7e10f85
cyjadela <adelacho@naver.com>
2025-01-17 13:25:56 +0900

    Chore: Goods 상품 조회 로직 삭제

--- src/main/java/com/example/demo/service/GoodsService.java
+++ src/main/java/com/example/demo/service/GoodsService.java
@@ -1,23 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.GoodsDto;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GoodsService {
 
-    String appRunType;
-
-    public GoodsService(@Value("default") String appRunType) { this.appRunType = appRunType; }
-
-    public GoodsDto getGoodsByGoodsNo(String goodsNo) {
-
-        GoodsDto goodsDto = GoodsDto.builder()
-                .goodsNo(goodsNo)
-                .goodsName("goods-" + appRunType + "-"+goodsNo)
-                .build();
-
-        return goodsDto;
-    }
 }
